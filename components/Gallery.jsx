90781d5826917cf548e958495f34f59229a124e4
Gilbert Hyman Goes <gilberthg@fairtech.com.sg>
2024-02-08 17:03:35 +0800

    Remove highlighted image when it is on search

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -150,6 +150,8 @@ const Gallery = ({ userId }) => {
 	 */
 	useEffect(() => {
 		processFetchImages(true);
+		setHighlightedImageIndex(null);
+		setHighlightedImage(null);
 	}, [sourceType, userId, query]);
 	
 	/**
